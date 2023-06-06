7ebc593f799121f9fdf5cd36d2cb5d2a0de6ecc4
RuthRiv910 <134336745+RuthRiv910@users.noreply.github.com>
2023-06-06 16:33:47 -0700

    Update bubbleSort.asm

--- bubbleSort.asm
+++ bubbleSort.asm
@@ -11,23 +11,23 @@
 
 ; Count the number of items to be sorted and store the value in R7
 
-            AND     R2, R2, #0  ; Initialize R2 <- 0 (counter)
-            LD      R3, FILE    ; Put file pointer into R3
-COUNT       LDR     R0, R3, #0  ; Put next file item into R0
+            AND     R2, R2, #0  ;  R2 <- 0 
+            LD      R3, FILE    ; file pointer into R3
+COUNT       LDR     R0, R3, #0  ; next file item into R0
             BRZ     END_COUNT   ; Loop until file item is 0
             ADD     R3, R3, #1  ; Increment file pointer
             ADD     R2, R2, #1  ; Increment counter
             BRNZP   COUNT       ; Counter loop
-END_COUNT   ADD     R4, R2, #0  ; Store total items in R4 (outer loop count)
+ENDCOUNT   ADD     R4, R2, #0  ; Store total items in R4 (outer loop count)
             BRZ     SORTED      ; Empty file
 
 ; Do the bubble sort
 
-OUTERLOOP   ADD     R4, R4, #-1 ; loop n - 1 times
+OUTLOOP   ADD     R4, R4, #-1 ; loop n - 1 times
             BRNZ    SORTED      ; Looping complete, exit
             ADD     R5, R4, #0  ; Initialize inner loop counter to outer
             LD      R3, FILE    ; Set file pointer to beginning of file
-INNERLOOP   LDR     R0, R3, #0  ; Get item at file pointer
+INNLOOP   LDR     R0, R3, #0  ; Get item at file pointer
             LDR     R1, R3, #1  ; Get next item
             NOT     R2, R1      ; Negate ...
             ADD     R2, R2, #1  ;        ... next item
@@ -35,11 +35,11 @@ INNERLOOP   LDR     R0, R3, #0  ; Get item at file pointer
             BRNZ    SWAPPED     ; Don't swap if in order (item <= next item)
             STR     R1, R3, #0  ; Perform ...
             STR     R0, R3, #1  ;         ... swap
-SWAPPED     ADD     R3, R3, #1  ; Increment file pointer
+SWAP     ADD     R3, R3, #1  ; Increment file pointer
             ADD     R5, R5, #-1 ; Decrement inner loop counter
             BRP     INNERLOOP   ; End of inner loop
             BRNZP   OUTERLOOP   ; End of outer loop
-SORTED      HALT
+SORT      HALT
 
 FILE        .FILL   x3500       ; File location
             .END
