cfd16c49356c7893c88ddb3c00e84875ff44d255
RuthRiv910 <134336745+RuthRiv910@users.noreply.github.com>
2023-06-06 16:26:58 -0700

    Update numbers.asm

--- numbers.asm
+++ numbers.asm
@@ -1,15 +1,9 @@
       .ORIG       x3500
-        .FILL       #10
-        .FILL       #7
-        .FILL       #4
-        .FILL       #1
-        .FILL       #5
+        .FILL       #11
         .FILL       #8
         .FILL       #2
-        .FILL       #11
-        .FILL       #20
-        .FILL       #3
+        .FILL       #17
         .FILL       #6
-        .FILL       #10
-        .FILL       #9
-        .END
+        .FILL       #4
+        .FILL       #3
+        .FILL       #21
